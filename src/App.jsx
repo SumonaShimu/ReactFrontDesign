febdc110c75a4316d306815f1b5b5ed1c4ddcad0
SumonaShimu <sumona59shimu@gmail.com>
2023-07-06 19:52:34 +0600

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-
 import './App.css'
 import Dashboard from './components/Dashboard';
-import Left from './components/Left';
 import NavBar from './components/NavBar';
-import Right from './components/Right';
 
 function App() {
 
   return (
-    // <div className="flex gap-5 justify-start">
-    //   <Left />
-    //   <Right />
-    // </div>
-    <div className='lg:w-[100%] lg:h-[100vh]'>
+    <div className='w-[100%] h-[100vh]'>
       <NavBar/>
       <Dashboard/>
     </div>
